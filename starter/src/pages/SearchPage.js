6b5a65df8371c74bc94b6695017b9d4b4badd640
elisenda <elisenda.monserrat@gmail.com>
2022-11-22 19:07:32 +0100

    feat: Main Routes definition and pages
    Wraps the main app with BrowserRouter and define the main Routes with their paths in App.js. Creates a component for each specific page of the app: pages/Home.js and pages/SearchPage.js

---
+++ starter/src/pages/SearchPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SearchPage = () => {
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <a href="#" className="close-search" onClick={() => {}}>
+          Close
+        </a>
+        <div className="search-books-input-wrapper">
+          <input type="text" placeholder="Search by title, author, or ISBN" />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
